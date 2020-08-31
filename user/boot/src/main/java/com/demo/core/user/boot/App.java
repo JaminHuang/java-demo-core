e1b1bb26cc2b2a5f112382ee9a56c19a287c28f1
jamin <jamin.huang@pdabc.com>
2020-08-31 18:18:27 +0800

    统一处理jar包版本

--- user/boot/src/main/java/com/demo/core/user/boot/App.java
+++ user/boot/src/main/java/com/demo/core/user/boot/App.java
@@ -1,9 +1,19 @@
 package com.demo.core.user.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableCaching
+@EnableEurekaClient
+@EnableTransactionManagement
 @SpringBootApplication
+@MapperScan({"com.demo.core.user.service.mapper","com.demo.sdk.mapper"})
+@ComponentScan(basePackages = {"com.demo"})
 public class App {
 
     public static void main(String[] args) {
