6ba3db12b08e741ad2ac5eff0cea80e5aac3c7c1
JaminHuang <huanghzm@126.com>
2021-08-19 16:41:48 +0800

    修改配置文件

--- user/boot/src/main/java/com/demo/core/user/boot/App.java
+++ user/boot/src/main/java/com/demo/core/user/boot/App.java
@@ -1,6 +1,7 @@
 package com.demo.core.user.boot;
 
 import com.demo.ibatx.spring.annotation.ExtMapperScan;
+import com.demo.sdk.apollo.EnableMyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableEurekaClient
 @EnableTransactionManagement
 @SpringBootApplication
+@EnableMyConfig
 @ExtMapperScan({"com.demo.core.user.service.mapper"})
 @ComponentScan(basePackages = {"com.demo"})
 public class App {
